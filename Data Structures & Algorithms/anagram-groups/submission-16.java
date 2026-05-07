f53c5b6afc02ea148aeff81de352b019c9536b20
Ralph Daou <ralph.daou5@gmail.com>
2026-05-07 09:51:16 +0300

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
+            
+            if(map.containsKey(str)){
+                map.get(str).add(s);
+            }
+            else{
+                map.put(str,new ArrayList<>(List.of(s)));
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        for(String s : map.keySet()){
+            res.add(map.get(s));
+        }
+        return res;
+    }
+}
