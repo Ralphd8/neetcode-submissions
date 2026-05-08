366b76c190a68ea897563fd3cbef05fec2f35c74
Ralph Daou <ralph.daou5@gmail.com>
2026-05-08 08:33:54 +0300

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() == 0) return true;  
+        if(s.length()%2 == 1) return false;
+
+        Deque<Character> dq = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()){
+            if((c == '}' || c == ')' || c == ']') && dq.isEmpty()){return false;}
+            else if(c == '(' || c == '[' || c == '{'){dq.addFirst(c);}
+            else if((c == '}' && dq.peekFirst() == '{') || 
+                    (c == ']' && dq.peekFirst() == '[') || 
+                    (c == ')' && dq.peekFirst() == '(')
+                    ){
+                        dq.removeFirst();
+                    }
+            else{return false;}
+        }
+        if(dq.isEmpty()) return true;
+        return false;
+    }
+}
