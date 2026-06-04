a52a9c843295675d87cc1655c838135326674f59
Ralph Daou <ralph.daou5@gmail.com>
2026-06-04 16:35:52 +0300

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals,new Comparator<Interval>(){
+            public int compare(Interval l1,Interval l2){
+                return l1.start - l2.start;
+            }
+        });
+        for(int i = 0;i<intervals.size()-1;i++){
+            if(intervals.get(i).end > intervals.get(i+1).start){
+                return false;
+            }
+        } 
+        return true;
+    }
+}
